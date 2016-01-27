91f896a36cffa94b49ac4c1dbeaa19bb7c4ba94c
Rodz3rd <shinichiConan3rd@gmail.com>
2016-01-26 19:06:27 -0800

    finish admin ui

---
+++ assets/js/admin.js
@@ -0,0 +1,14 @@
+$( function () {
+
+// put theme list
+var themes = ['black-tie', 'blitzer', 'cupertino', 'dark-hive', 'dot-luv', 'eggplant',
+			  'excite-bike', 'flick', 'hot-sneaks', 'humanity', 'le-frog', 'mint-choc',
+			  'overcast', 'pepper-grinder', 'redmond', 'smoothness', 'south-street',
+			  'start', 'sunny', 'swanky-purse', 'trontastic', 'ui-darkness', 'ui-lightness',
+			  'vader'];
+str = "";
+for ( i = 0; i < themes.length; i++ ) {
+	str += "<option>"+themes[i]+"</option>";
+}
+$('#themes').html(str);
+});
